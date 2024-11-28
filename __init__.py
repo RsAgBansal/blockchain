625a4ab8121e0a7718293d17072f604bbe40ffd8
Rishi Agarwal <23ucs185@lnmiit.ac.in>
2024-11-28 11:00:47 +0530

    Added result functionality

---
+++ __init__.py
@@ -0,0 +1,3 @@
+from .deploy_contract import deploy_survey_contract
+from .check_survey_results import get_survey_results
+#to be continued
--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import base64
 from flask import Flask, request, jsonify, send_from_directory
 from algosdk import account, mnemonic
 from algosdk.transaction import ApplicationNoOpTxn
 from algosdk.v2client import algod
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key
 from deploy_contract import deploy_survey_contract
 
 load_dotenv()
@@ -15,10 +16,16 @@ algod_token = os.getenv("TESTNET_ALGOD_TOKEN")
 algod_url = os.getenv("TESTNET_ALGOD_URL")
 algod_client = algod.AlgodClient(algod_token, algod_url)
 
+
 # User's private key and application ID
 user_private_key = os.getenv("PRIVATE_KEY")
 user_address = os.getenv("ADDRESS")
 app_id = int(os.getenv("APP_ID"))
+
+def update_env_file(key, value):
+    env_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+    set_key(env_path, key, value)
+    
 
 @app.route('/')
 def serve_index():
@@ -51,11 +58,12 @@ def create_survey():
     #signed_txn = txn.sign(user_private_key)
    # txid = algod_client.send_transaction(signed_txn)
     #return jsonify(txid=txid)
-    question = "What is your favorite color?"
-    option1 = "Blue"
-    option2 = "Green"
+    question = ""
+    option1 = ""
+    option2 = ""
 
     app_id = deploy_survey_contract(question, option1, option2)
+    update_env_file('APP_ID', str(app_id))
     return jsonify({'app_id': app_id})
 
 @app.route('/submit_response', methods=['POST'])
@@ -76,6 +84,20 @@ def submit_response():
     signed_txn = txn.sign(user_private_key)
     txid = algod_client.send_transaction(signed_txn)
     return jsonify(txid=txid)
+
+@app.route('/survey_results', methods=['GET'])
+def survey_results():
+    results = algod_client.application_info(app_id)
+    global_state = results['params']['global-state']
+    response1 = 0
+    response2 = 0
+    for item in global_state:
+        key = base64.b64decode(item['key']).decode('utf-8')
+        if key == 'Response1':
+            response1 = item['value']['uint']
+        elif key == 'Response2':
+            response2 = item['value']['uint']
+    return jsonify({'Response1': response1, 'Response2': response2})
 
 if __name__ == '__main__':
     app.run(debug=True)
